b8724abefa7ed618a1beff725da53c221f7732d2
Joseph <jkahn@waveaccounting.com>
2014-06-30 12:42:29 -0400

    attempt to fix

--- flake8_print.py
+++ flake8_print.py
@@ -1,16 +1,18 @@
 import pep8
 import re
 
-__version__ = '1.0'
+__version__ = '1.1'
 
 PRINT_REGEX = re.compile(r'(print)')
 
-def check_for_print_statements(line_of_code):
-    if pep8.noqa(line_of_code):
+
+def check_for_print_statements(physical_line):
+    if pep8.noqa(physical_line):
         return
-    print_match = PRINT_REGEX.search(line_of_code)
-    if print_match:
-        return print_match.start(), 'T000 print statement found.'
+    match = PRINT_REGEX.search(physical_line)
+    if match:
+        return match.start(), 'T000 print statement found.'
+
 
 check_for_print_statements.name = name = 'flake8-print'
 check_for_print_statements.version = __version__
